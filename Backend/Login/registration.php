87366432c55745b35c6950d156d83cba00169dba
paul asi caranto <paulasi.crnt@gmail.com>
2023-05-09 20:03:06 +0800

    Commit

--- Backend/Login/registration.php
+++ Backend/Login/registration.php
@@ -6,8 +6,8 @@
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="registration.scss" rel='stylesheet'>
-        <title>CHALAROSTE | Registration. Book your place now!</title>
+        <link href="registration.css" rel='stylesheet'>
+        <title>Registration</title>
     </head>
 </html>
 <?php
