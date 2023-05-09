422a42b949fcbd12cf2a33ed29ae13ace0fbe9e6
Kizzel Mae <80991719+zrnae@users.noreply.github.com>
2023-05-09 20:26:30 +0800

    Added new .scss; Editing Admin dashboard

--- Backend/Login/registration.php
+++ Backend/Login/registration.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="registration.scss" rel='stylesheet'>
+        <link href="style/registration.scss" rel='stylesheet'>
         <title>CHALAROSTE | Registration. Book your place now!</title>
     </head>
 </html>
